0e9d7e528a4d253ab1d41ddf43517b72966a7916
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-18 18:18:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -92,7 +92,7 @@ def adding_matz
 		 #programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 #programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
 		 #programmer_hash[:yukihiro_matsumoto][:languages] << "C"
-		 programmer_hash[":yukihiro_matsumoto"] => [""]
+		 programmer_hash[':yukihiro_matsumoto'] => [""]
 		 programmer_hash[:yukihiro_matsumoto] << {:known_for => ["Ruby"] ,:languages => ["LISP", "C"]}
 
 		# return programmer_hash[:yukihiro_matsumoto]
